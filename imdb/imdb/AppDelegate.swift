99e357bedb0bac884943586432f15aa194fab63c
Saqib Ali <saqibali773@gmail.com>
2022-04-17 01:12:58 +0500

    Project setup

---
+++ imdb/imdb/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  imdb
+//
+//  Created by Saqib Ali on 17/04/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ imdb/imdb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imdb
+//
+//  Created by Saqib Ali on 17/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
